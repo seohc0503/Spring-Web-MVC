fc78eac39ded61a5ff13c9aa06ada9e5aa69b2b1
HEECHANG SEO <seohc0503@naver.com>
2023-10-14 22:38:32 +0900

    Servlet - Request/Response

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -17,6 +17,7 @@ public class RequestHeaderServlet extends HttpServlet {
         printStartLine(request);
         printHeaders(request);
         printHeaderUtils(request);
+        printEtc(request);
     }
 
     private static void printStartLine(HttpServletRequest request) {
